e9d54cfa6c1a84a8aca39a226080ddd0570d351b
Mandeep kaur <1402mandeepkaur@gmail.com>
2025-07-28 15:30:48 +0530

    return

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const bookRoutes=require("./routes/addBookRoutes")
 const searchBooks = require("./routes/opacRoutes")
 const issueRoutes = require("./routes/issueRoutes")
 
+
 //load env var
 dotenv.config()
 app.use(cors())
@@ -20,6 +21,7 @@ app.use("/admin",adminRoutes)
 app.use("/library",bookRoutes)
 app.use("/opac",searchBooks)
 app.use("/issue",issueRoutes)
+
 const port=process.env.PORT||3000
 app.listen(port,()=>{
     console.log(`Server running on http://localhost:${port}`)
